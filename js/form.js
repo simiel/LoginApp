5a8a6ced36fcf3ee88bbc1f7c6f9bd2452c39929
unknown <samensah036@st.ug.edu.gh>
2023-02-15 12:12:23 +0000

    Completed all pages with animation

---
+++ js/form.js
@@ -0,0 +1,9 @@
+// form load animation
+
+const form = [...document.querySelector('.form').children]
+
+form.forEach( (item, i) => {
+    setTimeout( () => {
+        item.style.opacity = 1
+    }, i*100)
+})
