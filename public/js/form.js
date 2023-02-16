298f6223cc372acf971b78804dc67bead3ebf403
unknown <samensah036@st.ug.edu.gh>
2023-02-16 13:15:51 +0000

    database, server, and frontend working as expected with bugs fixed. Just basic workflow

--- public/js/form.js
+++ public/js/form.js
@@ -7,3 +7,54 @@ form.forEach( (item, i) => {
         item.style.opacity = 1
     }, i*100)
 })
+
+
+const name = document.querySelector('.name')
+const email = document.querySelector('.email')
+const password = document.querySelector('.password')
+const submitBtn = document.querySelector('.submit-btn')
+
+if(name == null){
+    //login page
+    submitBtn.addEventListener('click', ()=>{
+        fetch('/login-user', 
+        {
+            method: 'post',
+            headers: new Headers({'content-type':'application/json'}),
+            body: JSON.stringify({email: email.value, password: password.value})
+        })
+        .then(
+            res => res.json()
+        )
+        .then(
+            data => {
+                if(data.name){
+                    alert('login successful')
+                } else{
+                    alert(data)
+                }
+            }
+        )
+    })
+} else{
+    // register page
+
+    submitBtn.addEventListener('click', () => {
+        fetch('/register-user',{
+            method: 'post',
+            headers: new Headers({'content-type':'application/json'}),
+            body: JSON.stringify({
+                name : name.value,
+                email: email.value,
+                password: password.value,
+            })
+        }).then( res => res.json())
+        .then( data => {
+            if(data.name){
+                alert('register successful')
+            } else{
+                alert(data)
+            }
+        })
+    })
+}
--- server.js
+++ server.js
@@ -4,10 +4,21 @@ const knex = require('knex')
 const bodyParser = require('body-parser')
 
 
+db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'admin',
+        database: 'loginapp',
+    }
+})
+
 app = express()
 
 let initialPath = path.join(__dirname, "public")
 
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(express.static(initialPath))
 
@@ -19,12 +30,56 @@ app.get('/', (req,res) => {
 app.get('/login', (req,res) => {
     res.sendFile(path.join(initialPath,"login.html"))
 })
+
 app.get('/register', (req,res) => {
     res.sendFile(path.join(initialPath,"register.html"))
+})
+
+app.post('/register-user', (req,res)=>{
+    const {name, email, password} = req.body
+    console.log(name)
+    if(!name.length || !email.length || !password.length){
+        res.json("fill all the fields")
+    } else{
+        db('users').insert(
+            {
+                name: name,
+                email: email,
+                password: password
+            }
+        )
+        .returning(['name', 'email'])
+        .then( data=> res.json(data[0]))
+        .catch( e =>{
+            if(e.detail.includes('already exists')){
+                res.json('email already exists')
+            }
+        })
+    }
+    
+})
+
+app.post('/login-user', (req, res)=>{
+    const {password, email} = req.body
+    db.select('name', 'email')
+    .from('users')
+    .where({
+        email: email,
+        password: password
+    })
+    .then(
+        data => {
+            if(data.length){
+                res.json(data[0])
+            } else{
+                res.json('email or password is incorect')
+            }
+        }
+    )
 })
 
 
 
 app.listen(3000, (req,res) => {
-    console.log('listening on port 3000 ... I am tired')
+    console.log("listening on port 3000 ... let's geaux")
 })
